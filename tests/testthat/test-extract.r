583cd3b49d1467b69d02137bfbc19151d05edf20
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-06-04 18:43:19 -0700

    add more test for extraction

--- tests/testthat/test-extract.r
+++ tests/testthat/test-extract.r
@@ -34,18 +34,57 @@ test_that("extraction errors", {
 })
 
 
-test_that("parents are extracted", {
+test_that("user-defined parents are extracted", {
   expect_parents <- function (expr, env, expected) {
+    expr    <- substitute(expr)
     parents <- extract_parents(expr, env)
     expect_equal(env, expected)
   }
+
+  env <- list(a = 1)
+  expect_parent( a , env, env)
   
+  # function call
+  env <- list(a = 1, b = 2)
+  expect_parent( f(a, b) , env, env)
+  
+  # missing parent
+  env <- list(a = 1)
+  expect_parent( f(a, b) , env, env)
+  
+  # function
+  env <- list(a = 1, f = function(...)NULL)
+  expect_parent( f(a) , env, env)
+  
+  # recursive calls
   env <- list(a = 1, b = 2, c = 4, d = 5, e = 6, f = function(...)NULL)
-  expect_parents(quote(f(a, b, g(c, d), 3, z = e)), env, env)
+  expect_parents( f(a, b, g(c, d), 3, z = e) , env, env)
+
+  # dplyr-like verbs & lazy evaluation
+  env <- list(a = 1, b = 2)
+  expect_parents( iris %>% select(Species) %>% summary , env, env)
 })
 
 
+# TODO make replacement of literals optional?
+test_that("literals are substituted", {
+  expect_literals <- function (input, subst, output) {
+    result <- replace_literals(substitute(input), subst)
+    expected <- substitute(output)
+    expect_equal(result, expected)
+  }
+  
+  expect_literals( f(a, b) , list(a = 1), f(1, b) )
+  expect_literals( f(g(h(a))) , list(a = 1), f(g(h(1))) )
+  expect_literals(iris %>% filter(Sepal.Width > a), list(a = 1),
+                  iris %>% filter(Sepal.Width > 1))
+})
 
 
-
+test_that("package dependencies (aka parents) are extracted", {
+  skip_if_not_installed(dplyr)
+  
+  expect_pkg_deps(iris %>% filter(Sepal.Width > a),
+                  list("iris", "filter"))
+})
 
