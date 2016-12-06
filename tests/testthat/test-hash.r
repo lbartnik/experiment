29e6638b5eff2a0e903e668477461fb3fbcefe3c
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-12-05 23:42:52 -0800

    clean old code

--- tests/testthat/test-hash.r
+++
@@ -1,8 +0,0 @@
-context("hash")
-
-
-test_that("shorten hash", {
-  expect_equal(shorten("abcd"), "abcd")
-  expect_equal(shorten("abcdefgh"), "abcdefgh")
-  expect_equal(shorten("abcdefgh000000000"), "abcdefgh")
-})
