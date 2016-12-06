87b5f8382f718b4c851f2ce27e0e362298383faf
Lukasz A. Bartnik <l.bartnik@gmail.com>
2016-12-05 23:50:38 -0800

    start a new package

---
+++ R/compare.R
@@ -0,0 +1,30 @@
+#' @export
+compare_models <- function (x, ...) UseMethod("compare_models")
+
+
+#' @export
+#' @rdname compare_models
+compare_models.default <- function (x, ...)
+{
+  models <- c(list(x), list(...))
+  # TODO make sure all objects are actual models
+  #      and then call the .list method
+}
+
+
+#' @export
+#' @rdname compare_models
+compare_models.clist <- function (x, ...)
+{
+  # TODO extract into a regular list a then call the .list method
+}
+
+
+#' @export
+#' @rdname compare_models
+compare_models.list <- function (x, ...)
+{
+  # TODO print all models in a scatter-plot
+}
+
+
