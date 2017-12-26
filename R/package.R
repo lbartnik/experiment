aa86334a21a8650595723eedde71313eb1e971f2
Lukasz A. Bartnik <l.bartnik@gmail.com>
2017-12-25 23:06:16 -0800

    redo tracking

--- R/package.R
+++ R/package.R
@@ -5,25 +5,9 @@ NULL
 .onLoad <- function (libname, pkgname)
 {
   initiate_state()
-  internal_state$task_callback_id <- addTaskCallback(task_callback)
-
-  if (interactive() && FALSE) {
-    tracking_on()
-  }
 }
-
 
 .onUnload <- function (libpath)
 {
-  if (interactive()) {
-    if (!is.na(internal_state$task_callback_id)) {
-      removeTaskCallback(internal_state$task_callback_id)
-      internal_state$task_callback_id <- NA
-    }
-
-    tracking_off()
-    internal_state$old_prompt <- NA
-  }
+  tracking_off()
 }
-
-
