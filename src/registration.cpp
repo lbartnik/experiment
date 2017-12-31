43eae9c7d2ad091e67c6a4a94f2987e687b20bf1
Lukasz A. Bartnik <l.bartnik@gmail.com>
2017-12-31 09:58:16 -0800

    add C++ code

---
+++ src/registration.cpp
@@ -0,0 +1,17 @@
+// RegisteringDynamic Symbols
+
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+static const R_CallMethodDef callMethods[]  = {
+  { "C_try_readline", (DL_FUNC) &C_try_readline, 0 },
+  { NULL, NULL, 0 }
+};
+
+
+void R_init_experiment(DllInfo* info) {
+  R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+  R_useDynamicSymbols(info, TRUE);
+}
+
